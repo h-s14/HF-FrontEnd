2277709e3c83412dfd59640976030fb5a6774103
Harry <105970544+h-s14@users.noreply.github.com>
2024-10-26 21:57:46 +0530

    -backend-url-test-2-

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ const App = () => {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          `${backendUrl}/api/v1/user/patient/me`,
+          `${backendUrl}:4000/api/v1/user/patient/me`,
           // `http://localhost:4000/api/v1/user/patient/me`,
           { withCredentials: true },
         );
